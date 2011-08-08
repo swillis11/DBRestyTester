97605ff3f267fa7a3166232e76ab93406293d70d
Patrick McFadin <pmcfadin@gmail.com>
2011-08-08 12:51:47 -0700

    Initial commit

---
+++ src/com/hobsons/db/test/DBRestyTester.java
@@ -0,0 +1,26 @@
+package com.hobsons.db.test;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+import com.hobsons.servlet.rest.*;
+
+public class DBRestyTester {
+
+	public static void main(String[] args) throws Exception {
+        Server server = new Server(8088);
+        
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/");
+        server.setHandler(context);
+        
+        context.addServlet(new ServletHolder(new CassandraHandlerServlet()), "/cassandrahandlerservlet");
+        
+        System.out.println("REST server started");
+        
+        server.start();
+        server.join();
+        
+	}
+}
