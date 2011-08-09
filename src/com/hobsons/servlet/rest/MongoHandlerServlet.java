2a4be12d59df7b8ee1a6eba99dd6a0a57a1f0d05
swillis <swillis@hornet>
2011-08-09 14:30:29 -0700

    Mongo connects to the database

--- src/com/hobsons/servlet/rest/MongoHandlerServlet.java
+++ src/com/hobsons/servlet/rest/MongoHandlerServlet.java
@@ -3,12 +3,15 @@ package com.hobsons.servlet.rest;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Map;
+import java.util.Set;
 import java.util.UUID;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import me.prettyprint.hector.api.exceptions.HectorException;
 
 import com.mongodb.Mongo;
 import com.mongodb.DB;
@@ -17,36 +20,49 @@ import com.mongodb.BasicDBObject;
 import com.mongodb.DBObject;
 import com.mongodb.DBCursor;
 
-public class MongoHandlerServlet extends HttpServlet {
-	
-	
+
+@SuppressWarnings("serial")
+public class MongoHandlerServlet extends HttpServlet{
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		super.doGet(req, resp);
+
+		
+		try{
+			Mongo m = new Mongo("graylog2",27017);
+			DB db = m.getDB( "mydb" );
+			
+			Set<String> colls = db.getCollectionNames();
+	
+			for (String s : colls) {
+			    System.out.println(s);
+			}
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		
 	}
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		super.doPost(req, resp);
+		Mongo m = new Mongo("graylog2",27017);
+		DB db = m.getDB( "mydb" );
 	}
 
 	@Override
 	protected void doPut(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		super.doPut(req, resp);
+		Mongo m = new Mongo("graylog2",27017);
+		DB db = m.getDB( "mydb" );
 	}
 
 	@Override
 	protected void doDelete(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		super.doDelete(req, resp);
+		Mongo m = new Mongo("graylog2",27017);
+		DB db = m.getDB( "mydb" );
 	}
 
 }
